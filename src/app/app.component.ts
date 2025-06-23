c574e31e55507eacaacd94ce6c7b3a95166309a2
ansar <ansarsaudagar40@gmail.com>
2025-06-23 15:23:11 +0530

    basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AuthenticationComponent } from './layout/components/authentication/authentication.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AuthenticationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
