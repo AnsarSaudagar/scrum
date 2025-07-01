d2773c2f29b58a7ef5f890dd877c1c00c012ecf1
ansar <ansarsaudagar40@gmail.com>
2025-07-02 00:04:29 +0530

    org nav component

---
+++ src/app/features/organizations/org-nav/org-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-org-nav',
+  imports: [ButtonModule],
+  templateUrl: './org-nav.component.html',
+  styleUrl: './org-nav.component.css'
+})
+export class OrgNavComponent {
+
+}
--- src/app/features/organizations/organizations.component.ts
+++ src/app/features/organizations/organizations.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../../shared/components/logo/logo.component';
 import { OrgHeaderComponent } from './org-header/org-header.component';
+import { ButtonModule } from 'primeng/button';
+import { OrgNavComponent } from './org-nav/org-nav.component';
 
 @Component({
   selector: 'app-organizations',
-  imports: [OrgHeaderComponent],
+  imports: [OrgHeaderComponent, OrgNavComponent],
   templateUrl: './organizations.component.html',
   styleUrl: './organizations.component.css'
 })
