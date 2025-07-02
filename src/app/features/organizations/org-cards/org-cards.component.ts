f6e3109e6026c83be5876ab7a33a0014d234251a
ansar <ansarsaudagar40@gmail.com>
2025-07-02 11:15:42 +0530

    progress bar added

--- src/app/features/organizations/org-cards/org-cards.component.ts
+++ src/app/features/organizations/org-cards/org-cards.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-org-cards',
-  imports: [],
+  imports: [ProgressBarModule],
   templateUrl: './org-cards.component.html',
   styleUrl: './org-cards.component.css'
 })
