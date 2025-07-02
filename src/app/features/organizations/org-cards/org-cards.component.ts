140ec28a81738a5d82366bbbb1a1cbf863eb2da3
ansar <ansarsaudagar40@gmail.com>
2025-07-02 10:59:31 +0530

    new component for cards

---
+++ src/app/features/organizations/org-cards/org-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-org-cards',
+  imports: [],
+  templateUrl: './org-cards.component.html',
+  styleUrl: './org-cards.component.css'
+})
+export class OrgCardsComponent {
+
+}
--- src/app/features/organizations/organizations.component.ts
+++ src/app/features/organizations/organizations.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { OrgHeaderComponent } from './org-header/org-header.component';
 import { OrgNavComponent } from './org-nav/org-nav.component';
 import { TagModule } from 'primeng/tag';
+import { OrgCardsComponent } from './org-cards/org-cards.component';
 
 @Component({
   selector: 'app-organizations',
-  imports: [OrgHeaderComponent, OrgNavComponent, TagModule],
+  imports: [OrgHeaderComponent, OrgNavComponent, OrgCardsComponent, TagModule],
   templateUrl: './organizations.component.html',
   styleUrl: './organizations.component.css'
 })
