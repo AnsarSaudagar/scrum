69163774d7e05ae4e61dd63e778a17711387b7dc
ansar <ansarsaudagar40@gmail.com>
2025-07-02 11:52:50 +0530

    Org card ready

--- src/app/features/organizations/org-cards/org-cards.component.ts
+++ src/app/features/organizations/org-cards/org-cards.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProgressBarModule } from 'primeng/progressbar';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-org-cards',
-  imports: [ProgressBarModule],
+  imports: [ProgressBarModule, DividerModule],
   templateUrl: './org-cards.component.html',
   styleUrl: './org-cards.component.css'
 })
