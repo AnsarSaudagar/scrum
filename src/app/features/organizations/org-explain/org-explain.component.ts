e0d7ac40fb4bc88c558da8dfffcdb3db12d5dfb7
ansar <ansarsaudagar40@gmail.com>
2025-07-05 00:57:52 +0530

    org explaination startec

---
+++ src/app/features/organizations/org-explain/org-explain.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-org-explain',
+  imports: [],
+  templateUrl: './org-explain.component.html',
+  styleUrl: './org-explain.component.css'
+})
+export class OrgExplainComponent {
+
+}
--- src/app/features/organizations/organizations.component.ts
+++ src/app/features/organizations/organizations.component.ts
@@ -3,10 +3,11 @@ import { OrgHeaderComponent } from './org-header/org-header.component';
 import { OrgNavComponent } from './org-nav/org-nav.component';
 import { TagModule } from 'primeng/tag';
 import { OrgCardsComponent } from './org-cards/org-cards.component';
+import { OrgExplainComponent } from './org-explain/org-explain.component';
 
 @Component({
   selector: 'app-organizations',
-  imports: [OrgHeaderComponent, OrgNavComponent, OrgCardsComponent, TagModule],
+  imports: [OrgHeaderComponent, OrgNavComponent, OrgCardsComponent, TagModule, OrgExplainComponent],
   templateUrl: './organizations.component.html',
   styleUrl: './organizations.component.css'
 })
