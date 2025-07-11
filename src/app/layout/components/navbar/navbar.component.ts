b1780b74912c80751c7ed5cd63bd5d60b223c81e
ansar <ansarsaudagar40@gmail.com>
2025-07-11 18:37:56 +0530

    basic layout css changes

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { OrgDialogComponent } from '../../../features/organizations/org-dialog/org-dialog.component';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-navbar',
-  imports: [OrgDialogComponent],
+  imports: [OrgDialogComponent, BadgeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
