e19bb177db049d6f3f4b88ab873e2f547843f09d
ansar <ansarsaudagar40@gmail.com>
2025-07-11 16:33:52 +0530

    search org input like created

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { OrgDialogComponent } from '../../../features/organizations/org-dialog/org-dialog.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [OrgDialogComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  visible: boolean = false;
+  
+  showOrgDialog() {
+    this.visible = true;
+  }
 }
