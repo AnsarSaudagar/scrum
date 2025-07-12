1b23e40d423aaa452bbac2174b0723739cc636fe
ansar <ansarsaudagar40@gmail.com>
2025-07-12 19:17:21 +0530

    color code refractoring and basic component creation

---
+++ src/app/features/organizations/org-dialog/join-tab/join-tab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-tab',
+  imports: [],
+  templateUrl: './join-tab.component.html',
+  styleUrl: './join-tab.component.css'
+})
+export class JoinTabComponent {
+
+}
--- src/app/features/organizations/org-dialog/org-dialog.component.ts
+++ src/app/features/organizations/org-dialog/org-dialog.component.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
-import { TabsModule } from 'primeng/tabs';
 import { CreateTabComponent } from './create-tab/create-tab.component';
 import { CustomTabsComponent } from '../../../shared/components/custom-tabs/custom-tabs.component';
+import { JoinTabComponent } from './join-tab/join-tab.component';
 
 @Component({
   selector: 'app-org-dialog',
-  imports: [TabsModule, DialogModule, CreateTabComponent, CustomTabsComponent],
+  imports: [DialogModule, CreateTabComponent, CustomTabsComponent, JoinTabComponent],
   templateUrl: './org-dialog.component.html',
   styleUrl: './org-dialog.component.css',
 })
